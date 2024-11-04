5440ee632277983287985ae410aa43a804be5799
Lucas S <lucassaraiva5@hotmail.com>
2024-11-04 14:25:35 -0300

    added fix

--- database/migrations/2024_11_04_170847_modify_avatar_column_on_users_table.php
+++ database/migrations/2024_11_04_170847_modify_avatar_column_on_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('avatar')->change(); // Altere 'users' para a sua tabela
+            $table->text('avatar')->nullable()->change(); // Altere 'users' para a sua tabela
         });
     }
 
