572d90a5f1de16d4c52020b0aaee4f926a4c6d52
Lucas S <lucassaraiva5@hotmail.com>
2024-11-04 13:12:07 -0300

    added changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Adicione outros estilos e links CSS aqui -->
 
     <!-- Styles / Scripts -->
