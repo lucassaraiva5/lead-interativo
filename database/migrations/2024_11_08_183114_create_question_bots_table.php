df9897d742278dc40c3f729d87cf3ecd87c1a10e
Lucas S <lucassaraiva5@hotmail.com>
2024-11-12 00:50:12 -0300

    added fix

--- database/migrations/2024_11_08_183114_create_question_bots_table.php
+++ database/migrations/2024_11_08_183114_create_question_bots_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('question_bots', function (Blueprint $table) {
             $table->id();
-            $table->string('question');
+            $table->text('question');
             $table->integer('order');
             $table->timestamps();
         });
